034627766b39733d0e04c714fe35d3b4fb668345
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-02-24 16:22:11 +0100

    Heading and input fix

--- src/components/ToDoList/TaskAddForm/StyledTaskAddForm.tsx
+++ src/components/ToDoList/TaskAddForm/StyledTaskAddForm.tsx
@@ -3,12 +3,9 @@ import styled from "styled-components";
 export const StyledToDoList = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 250px;
-  width: 100%;
 `
 export const StyledHeading = styled.h3`
   font-size: 30px;
   font-weight: bold;
   margin-bottom: 20px;
-  text-align: center;
 `
--- src/components/ToDoList/TaskAddForm/TaskAddForm.tsx
+++ src/components/ToDoList/TaskAddForm/TaskAddForm.tsx
@@ -1,8 +1,18 @@
+import styled from "styled-components";
+
 export const TaskAddForm = () => {
   return (
     <div>
-      <input type="text"/>
-      <button>+</button>
+      <StyledInputTask type="text"/>
+      <StyledBtnAdd>+</StyledBtnAdd>
     </div>
   );
 }
+
+export const StyledInputTask = styled.input`
+  padding: 10px 15px;
+  font-size: 14px;
+`
+export const StyledBtnAdd = styled.button`
+
+`

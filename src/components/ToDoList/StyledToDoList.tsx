59c3cfdfe5dc5017f72809b9422a7ad87301e317
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-02-24 17:06:07 +0100

    bg todolist

--- src/components/ToDoList/StyledToDoList.tsx
+++ src/components/ToDoList/StyledToDoList.tsx
@@ -3,8 +3,12 @@ import styled from "styled-components";
 export const StyledToDoList = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 250px;
+  max-width: 300px;
   width: 100%;
+  padding: 30px 20px;
+  background: rgba(0,0,0,.05);
+  border-radius: 3px;
+  overflow-y: scroll;
 `
 export const StyledHeading = styled.h3`
   font-size: 30px;

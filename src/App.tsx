a06758dfb7aec8af80f021f0d7a8bc83f97be5ac
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-02-24 18:47:54 +0100

    types for arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import {ToDoList} from "./components/ToDoList/ToDoList";
+import {TasksTypePropsType, ToDoList} from "./components/ToDoList/ToDoList";
 import {StyledApp} from "./StyledApp";
 
 function App() {
-  const tasks1 = [
+  const tasks1:Array<TasksTypePropsType> = [
     {id: 1, title: "HTML", isDone: true},
     {id: 2, title: "CSS", isDone: true},
     {id: 3, title: "JS", isDone: false},
   ];
 
-  const tasks2 = [
+  const tasks2:Array<TasksTypePropsType> = [
     {id: 1, title: "React", isDone: true},
     {id: 2, title: "Redux", isDone: true},
     {id: 3, title: "TypeScript", isDone: false}
